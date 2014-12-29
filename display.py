5e19129bd2f27a0ad63b38543daa2094765240bf
Tim Craig <timcraig@sonic.net>
2014-12-28 20:50:57 -0800

    Fixed up display size handling

--- display.py
+++ display.py
@@ -15,24 +15,23 @@ class Display(object):
 class PygameDisplay(Display):
     def __init__(self):
         self.display = None
-        self.display_info = None
+        self.display_size = None
         self.background = None
         self.mjpeg_streamer = None
 
     def init(self):
         pygame.init()
-        self.display_info = pygame.display.Info()
-        self.display = pygame.display.set_mode((self.display_info.current_w, self.display_info.current_h),
-                                               pygame.FULLSCREEN)
-        self.background = pygame.Surface((self.display_info.current_w, self.display_info.current_h))
+        display_info = pygame.display.Info()
+        self.display = pygame.display.set_mode((display_info.current_w, display_info.current_h), pygame.FULLSCREEN)
+        self.background = pygame.Surface(self.display.get_size())
         self.background.fill(pygame.Color('#E8E8E8'))
         self.display.blit(self.background, (0, 0))
         pygame.mouse.set_visible(False)
 
     def get_display_size(self):
         size = None
-        if self.display_info:
-            size = (self.display_info.current_w, self.display_info.current_h)
+        if self.display:
+            size = self.display.get_size()
         return size
 
     def set_stream(self, mjpeg_streamer):
@@ -41,6 +40,6 @@ class PygameDisplay(Display):
     def update(self):
         image = self.mjpeg_streamer.next_frame()
         image_panel = pygame.image.load(image).convert()
-        image_panel = pygame.transform.scale(image_panel, (self.display_info.current_w, self.display_info.current_h))
+        image_panel = pygame.transform.scale(image_panel, self.display.get_size())
         self.display.blit(image_panel, (0, 0))
         pygame.display.update()
